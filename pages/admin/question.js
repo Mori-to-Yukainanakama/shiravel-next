0021691b0f074290b7ba7793cf5630ae7571ff2e
morikouki <akzouksia@icloud.com>
2021-12-13 21:24:38 +0900

    管理者質問詳細画面編集

--- pages/admin/question.js
+++ pages/admin/question.js
@@ -6,51 +6,84 @@ export default function question() {
                 {`
                     .question__meta {
                         width: 100%;
-                        height: 500px;
+                        height: 200px;
                         background-color: yellow;
+                        padding: 30px;
+                        display: flex;
+                    }
+
+                    .question__meta--left {
+                        width: 60%;
+                    }
+
+                    .question__meta--right {
+                        width: 40%;
                     }
 
                     .question__status {
                         display: flex;
+                        align-items: flex-end;
+                        padding: 30px 0;
                     }
 
                     .question__status--list {
-                        margin: 10px 50px;
-                    }
-
-                    .question__status--list:last-child {
-                        margin-left: auto;
+                        margin: 0px 50px;
                     }
 
                     .question__title {
-                        padding: 50px 30px;
+                        padding: 30px 50px 0 50px;
                     }
 
                     .question__title--text {
-                        font-size: 60px;
+                        font-size: 50px;
                         font-weight: bold;
+                        border-bottom: 1px solid black;
                     }
 
                     .question__user {
+                        padding: 50px;
+                        margin-left: 50px;
                         display: flex;
-                        justify-content: flex-end;
-                        margin-right: 30px;
+                    }
+
+                    .question__user--name {
+                        margin: 20px;
+                        font-size: 24px;
+                    }
+
+                    .question__user--image {
+                        width: 60px;
+                        height: 60px;
+                        border-radius: 30px;
+                    }
+
+                    .question__body {
+                        height: 1000px;
+                        background-color: red;
                     }
                 `}
             </style>
             <h1>質問詳細画面</h1>
             <div className="question__meta">
-                <div className="question__status">
-                    <span className="question__status--list">解決済み</span>
-                    <span className="question__status--list">回答件数：3件</span>
-                    <span className="question__status--list">質問日：2021/12/25</span>
+                <div className="question__meta--left">
+                    <div className="question__title">
+                        <span className="question__title--text">Next.jsが難しい件について・・</span>
+                    </div>
+                    <div className="question__status">
+                        <span className="question__status--list">解決済</span>
+                        <span className="question__status--list">回答3</span>
+                        <span className="question__status--list">2021/12/25</span>
+                    </div>
                 </div>
-                <div className="question__title">
-                    <span className="question__title--text">Next.jsが難しい件について・・</span>
+                <div className="question__meta--right">
+                    <div className="question__user">
+                        <img className="question__user--image" src="/images/mori.jpeg" />
+                        <span className="question__user--name">モリモリ</span>
+                    </div>
                 </div>
-                <div className="question__user">
-                    <span className="question__user--name">投稿者：モリモリ</span>
-                </div>
+            </div>
+            <div className="question__body">
+
             </div>
         </div>
     );
