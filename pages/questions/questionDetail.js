6f368180732cbccebaded8324352b876e3094dee
Moony <moonys.ne.jp@gmail.com>
2021-12-18 22:40:09 +0900

    一時的なTopを作成。

--- pages/questions/questionDetail.js
+++ pages/questions/questionDetail.js
@@ -1,4 +1,11 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Box, Chip, Grid, Paper, Stack, Typography } from "@mui/material";
+
+const center_center = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 const questionDetail = () => {
   return (
@@ -6,27 +13,34 @@ const questionDetail = () => {
       {/* Top */}
       <Box
         sx={{
-          alignItems: "center",
-          bgcolor: "black",
-          display: "flex",
-          flexDirection: "column",
           height: 200,
-          justifyContent: "center",
+          bgcolor: "black",
+          ...center_center,
         }}
       >
+        <Typography variant="h3" component="h3" sx={{ color: "white", p: 1 }}>
+          Ask Whatever You Want To Know!
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          component="p"
+          sx={{ color: "white", p: 1 }}
+        >
+          知りたいことはなんでも聞け！
+        </Typography>
       </Box>
       {/* Top end */}
 
-      <Box sx={{ bgcolor: "orange", p: 2 }}>
+      <Box sx={{ bgcolor: "orange", p: 8 }}>
         <Grid container spacing={2}>
           {/* Heading */}
           <Grid item xs={12}>
             <Paper elevation={4} sx={{ bgcolor: "white" }}>
               <Grid container>
-                <Grid item xs={9} sx={{ height: 160, p: 2 }}>
+                <Grid item xs={9} sx={{ height: 160, p: 2}}>
                   <Box>text</Box>
                 </Grid>
-                <Grid item xs={3} sx={{ height: 160, p: 2 }}>
+                <Grid item xs={3} sx={{ height: 160, p: 2}}>
                   <Box>text</Box>
                 </Grid>
               </Grid>
