0ade9fe82c4287dbb570b5881ecbc16ff2fd762c
Moony <moonys.ne.jp@gmail.com>
2021-12-19 11:18:20 +0900

    Headingの大まかなレイアウトを作成。

--- pages/questions/questionDetail.js
+++ pages/questions/questionDetail.js
@@ -1,4 +1,14 @@
-import { Box, Chip, Grid, Paper, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Chip,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
+import Spacer from "../../components/spacer";
 
 const center_center = {
   display: "flex",
@@ -7,7 +17,13 @@ const center_center = {
   justifyContent: "center",
 };
 
-const questionDetail = () => {
+// const center_left = {
+//   display: "flex",
+//   flexDirection: "column",
+//   justifyContent: "center",
+// };
+
+const QuestionDetail = () => {
   return (
     <>
       {/* Top */}
@@ -31,42 +47,55 @@ const questionDetail = () => {
       </Box>
       {/* Top end */}
 
-      <Box sx={{ bgcolor: "orange", p: 8 }}>
-        <Grid container spacing={2}>
-          {/* Heading */}
-          <Grid item xs={12}>
-            <Paper elevation={4} sx={{ bgcolor: "white" }}>
-              <Grid container>
-                <Grid item xs={9} sx={{ height: 160, p: 2}}>
-                  <Box>text</Box>
+      <Box sx={{ bgcolor: "orange", py: 4 }}>
+        <Container>
+          <Grid container spacing={2}>
+            {/* Heading */}
+            <Grid item xs={12}>
+              <Paper elevation={4} sx={{ bgcolor: "white" }}>
+                <Grid container>
+                  <Grid item xs={9} sx={{ p: 3 }}>
+                    <Typography fontSize={8}>
+                      投稿日：2021年12月25日 12時25分
+                    </Typography>
+                    <Spacer height={8} />
+                    <Typography variant="h5" component="h5">
+                      Flaskにおける変数展開の使用方法
+                    </Typography>
+                    <Spacer height={12} />
+                    <Stack direction="row" spacing={1}>
+                      <Chip label="回答済" size="small" color="primary" />
+                      <Chip label="未解決" size="small" color="error" />
+                    </Stack>
+                  </Grid>
+                  <Grid item xs={3} sx={{ p: 3, ...center_center }}>
+                  <Chip avatar={<Avatar>K</Avatar>} label="Kohei" color="secondary"/>
+                  </Grid>
                 </Grid>
-                <Grid item xs={3} sx={{ height: 160, p: 2}}>
-                  <Box>text</Box>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-          {/* Heading end */}
+              </Paper>
+            </Grid>
+            {/* Heading end */}
 
-          {/* Main */}
-          <Grid item xs={9}>
-            <Paper elevation={4} sx={{ bgcolor: "white", height: 400, p: 2 }}>
-              <Box>Main</Box>
-            </Paper>
-          </Grid>
-          {/* Main end */}
+            {/* Main */}
+            <Grid item xs={9}>
+              <Paper elevation={4} sx={{ bgcolor: "white", height: 400, p: 2 }}>
+                <Box>Main</Box>
+              </Paper>
+            </Grid>
+            {/* Main end */}
 
-          {/* Sidebar */}
-          <Grid item xs={3}>
-            <Paper elevation={4} sx={{ bgcolor: "white", height: 400, p: 2 }}>
-              <Box>Sidebar</Box>
-            </Paper>
+            {/* Sidebar */}
+            <Grid item xs={3}>
+              <Paper elevation={4} sx={{ bgcolor: "white", height: 400, p: 2 }}>
+                <Box>Sidebar</Box>
+              </Paper>
+            </Grid>
+            {/* Sidebar end */}
           </Grid>
-          {/* Sidebar end */}
-        </Grid>
+        </Container>
       </Box>
     </>
   );
 };
 
-export default questionDetail;
+export default QuestionDetail;
