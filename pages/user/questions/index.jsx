3af8cf5257acf8361b695f9d38389c482c7769bb
shinya.fujii <shinya.fujii@fox-hound.tech>
2021-12-26 15:03:24 +0900

    質問一覧画面モック作成

---
+++ pages/user/questions/index.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import PageTitle from "../../../components/QuestionList/Atoms/PageTitle";
+import LabTabs from "../../../components/QuestionList/Organisms/LabTabs";
+
+export default function QuestionList() {
+  return (
+    <Box sx={{ padding: 5, backgroundColor: "#243661" }}>
+      <PageTitle title={"質問一覧"} />
+      <Grid container spacing={1} sx={{ justifyContent: "center" }}>
+        <Grid item xs={10}>
+          <LabTabs />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
