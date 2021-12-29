d6e1d76d3d785b6d79f71db870b2b798a351fedf
kumikoasano <kumiko.asano@fox_hound.tech>
2021-12-29 10:36:34 +0900

    js追記

--- pages/user/login.js
+++ pages/user/login.js
@@ -1,5 +1,27 @@
 export default () => (
   <div>
-    <p>This is the login page</p>
+    <main className="">
+      <div className="content">
+        <form>
+          <ul>
+            <li>
+              <span className="inputTitle">メールアドレス</span>
+              <input type="email" name="email" className="email" required />
+            </li>
+            <li>
+              <span className="inputTitle">パスワード</span>
+              <input
+                type="password"
+                name="password"
+                className="password"
+                required
+                minLength={8}
+              />
+            </li>
+          </ul>
+          <button type="submit">ログイン</button>
+        </form>
+      </div>
+    </main>
   </div>
 );
