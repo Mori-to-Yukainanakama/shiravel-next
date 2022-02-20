8fe72dea4736682518eeba37233d38b6fbb0f5dc
morikouki <akzouksia@icloud.com>
2022-02-20 21:20:50 +0900

    質問一覧　回答解決ステータス取得

--- components/Organisms/LabTabs.jsx
+++ components/Organisms/LabTabs.jsx
@@ -1,62 +1,109 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
+import Tabs from '@mui/material/Tabs';
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import ListCard from "../Molecules/ListCard";
+import { Card } from "@mui/material";
+import Spacer from "../Atoms/Spacer";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-export default function LabTabs() {
-  const [value, setValue] = React.useState("1");
+export default function LabTabs(props) {
+  const [value, setValue] = useState("1");
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    setQuestions(props.questions);
+  }, [props.questions]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const tabStyle = {
-    minWidth: "20%",
-    borderBottom: "solid",
-    backgroundColor: "#9ba0ab",
-    borderTopLeftRadius: "15px",
-    borderTopRightRadius: "15px",
-    "&:hover": {
-      backgroundColor: "#cf8807",
-    },
-  };
+  // 未回答 質問一覧取得
+  const getUnanswered = () => {
+    axios.get('http://localhost:8000/api/v1/questions/unanswered').then((response) => {
+      setQuestions(response.data);
+    });
+  }
+
+  // 回答有 質問一覧取得
+  const getAnswered = () => {
+    axios.get('http://localhost:8000/api/v1/questions/answered').then((response) => {
+      setQuestions(response.data);
+    });
+  }
+
+  // 未解決 質問一覧取得
+  const getUnsolved = () => {
+    axios.get('http://localhost:8000/api/v1/questions/unsolved').then((response) => {
+      setQuestions(response.data);
+    });
+  }
+
+  // 解決済 質問一覧取得
+  const getSolved = () => {
+    axios.get('http://localhost:8000/api/v1/questions/solved').then((response) => {
+      setQuestions(response.data);
+    });
+  }
 
   return (
     <Box
       sx={{
         width: "100%",
         height: "100%",
-        typography: "body1",
       }}
     >
       <TabContext value={value}>
-        <TabList onChange={handleChange} aria-label="lab API tabs example">
-          <Tab label="新着" value="1" sx={tabStyle} />
-          <Tab label="未回答" value="2" sx={tabStyle} />
-          <Tab label="回答済み" value="3" sx={tabStyle} />
-          <Tab label="未解決" value="4" sx={tabStyle} />
-          <Tab label="解決済み" value="5" sx={tabStyle} />
-        </TabList>
-        <Box sx={{ borderBottom: 1, backgroundColor: "#9ba0ab" }}>
-          <TabPanel value="1">
-            <ListCard label={"新着"} />
-          </TabPanel>
-          <TabPanel value="2">
-            <ListCard label={"未回答"} />
-          </TabPanel>
-          <TabPanel value="3">
-            <ListCard label={"回答済み"} />
-          </TabPanel>
-          <TabPanel value="4">
-            <ListCard label={"未解決"} />
-          </TabPanel>
-          <TabPanel value="5">
-            <ListCard label={"解決済み"} />
-          </TabPanel>
-        </Box>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="secondary tabs example"
+          TabIndicatorProps={{
+            style: {
+              height: '3px'
+            }
+          }}
+        >
+          <Tab label="新着" value="1" />
+          <Tab label="未回答" value="2" onClick={getUnanswered} />
+          <Tab label="回答済み" value="3" onClick={getAnswered} />
+          <Tab label="未解決" value="4" onClick={getUnsolved} />
+          <Tab label="解決済み" value="5" onClick={getSolved} />
+        </Tabs>
+        <Spacer whidth={"20px"} height={"20px"} />
+        <TabPanel value="1">
+          {questions.map((question, index) => {
+            return <ListCard question={question} key={index} />
+          })};
+        </TabPanel>
+        <TabPanel value="2">
+          {questions.map((question, index) => {
+            return <ListCard question={question} key={index} />
+          })};
+        </TabPanel>
+        <TabPanel value="3">
+          {questions.map((question, index) => {
+            return <ListCard question={question} key={index} />
+          })};
+        </TabPanel>
+        <TabPanel value="4">
+          {questions.map((question, index) => {
+            return <ListCard question={question} key={index} />
+          })};
+        </TabPanel>
+        <TabPanel value="5">
+          {questions.map((question, index) => {
+            return <ListCard question={question} key={index} />
+          })};
+        </TabPanel>
       </TabContext>
     </Box>
   );
