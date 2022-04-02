3a2fb7e4356e498a745e0acf700a8e5d296f16ab
shinya.fujii <shinya.fujii@fox-hound.tech>
2022-04-02 23:11:39 +0900

    質問投稿バリデーション設定

--- pages/user/questions/create.jsx
+++ pages/user/questions/create.jsx
@@ -1,4 +1,6 @@
 import { Paper } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
+
 import MarkdownEditor from "/components/Atoms/MarkdownEditor";
 import MarkdownPreview from "/components/Atoms/MarkdownPreview";
 import PostTitle from "/components/Molecules/CreateQuestion/PostTitle";
@@ -7,6 +9,12 @@ import PreviewButton from "/components/Molecules/CreateQuestion/PreviewButton";
 import PostQuestionButton from "/components/Molecules/CreateQuestion/PostQuestionButton";
 
 export default function create() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
   return (
     <>
       <CreateQuestionProvider>
@@ -15,11 +23,6 @@ export default function create() {
           <div className="editor">
             <MarkdownEditor />
           </div>
-          {/* <div className="editor">
-            <div className="preview">
-              <h2 className="title">プレビュー</h2>
-            </div>
-          </div> */}
         </Paper>
         <div className="btn-area">
           <PostQuestionButton text={"投稿する"} />
@@ -29,27 +32,10 @@ export default function create() {
       </CreateQuestionProvider>
 
       <style jsx>{`
-        .markdown-area {
-          width: 100%;
-          display: flex;
-          justify-content: center;
-        }
         .editor {
           width: 100%;
           height: 400px;
           padding: 0 0.4rem;
-        }
-        .editor h2 {
-          height: 10%;
-          padding: 0.3rem;
-          color: white;
-          background-color: #342950;
-        }
-        .preview {
-          width: 100%;
-          height: 100%;
-          box-sizing: border-box;
-          border: 0.1rem solid;
         }
         .btn-area {
           width: 100%;
